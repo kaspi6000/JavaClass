4a8817c3c2ab0b5e68d11f7dccc4b8f9b52e1568
kang <kaspi6000@github.com>
2018-02-06 17:46:12 +0900

    Add 0206(2)

--- Class/DB/script/JDBCTest/jdbc.sql
+++ Class/DB/script/JDBCTest/jdbc.sql
@@ -75,3 +75,109 @@ BEGIN
     COMMIT;
     --TRUNCATE TABLE tbladdress;
 END;
+
+-- m3()
+-- 반환값
+-- a. out 파라미터
+-- b. return 문
+
+CREATE OR REPLACE PROCEDURE proc_m3(
+    vcount OUT NUMBER
+)
+IS
+BEGIN
+    SELECT count(*) INTO vcount FROM tbladdress;
+END;
+
+DECLARE
+    vcount NUMBER;
+BEGIN
+    proc_m3(vcount);
+    DBMS_OUTPUT.put_line(vcount);
+END;
+
+CREATE OR REPLACE PROCEDURE proc_m3(
+    pseq IN NUMBER,
+    pname OUT VARCHAR2,
+    page OUT NUMBER,
+    ptel OUT VARCHAR2
+)
+IS
+BEGIN
+    SELECT name, age, tel INTO pname, page, ptel FROM tbladdress WHERE seq = pseq;
+END;
+
+INSERT INTO tbladdress VALUES(address_seq.nextval, '홍길동', 20, '010-1234-5678', '서울시', DEFAULT);
+
+SELECT * FROM tbladdress;
+
+CREATE OR REPLACE PROCEDURE proc_m4(
+    pcity VARCHAR2,
+    pcursor OUT SYS_REFCURSOR
+)
+IS
+BEGIN
+    OPEN pcursor FOR
+        SELECT name, buseo, jikwi FROM tblinsa WHERE city = pcity;
+END;
+
+CREATE OR REPLACE PROCEDURE proc_m5(
+    ptel VARCHAR2,
+    pcursor OUT SYS_REFCURSOR
+)
+IS
+BEGIN
+    OPEN pcursor FOR
+        SELECT name, buseo, jikwi, tel FROM tblinsa WHERE replace(tel, '-', '') LIKE '%' || ptel || '%';
+END;
+
+SELECT * FROM tblinsa;
+
+-----------------------------------------------------------------------------------------------------------------
+CREATE TABLE tblbuy(
+    seq NUMBER PRIMARY KEY,
+    name VARCHAR2(50) NOT NULL,
+    item VARCHAR2(50) NOT NULL,
+    qty NUMBER NOT NULL,
+    regdate DATE DEFAULT sysdate NOT NULL
+);
+
+CREATE SEQUENCE buyseq;
+DROP SEQUENCE buyseq;
+
+CREATE OR REPLACE PROCEDURE proc_addbuy(
+    pname VARCHAR2,
+    pitem VARCHAR2,
+    pqty NUMBER
+)
+IS
+    vcount NUMBER;
+BEGIN
+    SELECT count(*) INTO vcount FROM tblbuy WHERE name = pname AND item = pitem;
+    
+    IF vcount = 1 THEN
+        UPDATE tblbuy SET qty = pqty;
+    ELSE
+        INSERT INTO tblbuy VALUES(buyseq.nextval, pname, pitem, pqty, DEFAULT);
+    END IF;
+END;
+
+SELECT * FROM tblbuy;
+SELECT name || item FROM tblbuy;
+DELETE FROM tblbuy;
+INSERT INTO tblbuy VALUES(buyseq.nextval, '홍길동', '키보드', 8, DEFAULT);
+
+CREATE OR REPLACE PROCEDURE proc_listbuy(
+    pqty NUMBER,
+    pcursor OUT SYS_REFCURSOR
+)
+IS
+BEGIN
+    OPEN pcursor FOR
+        SELECT * FROM tblbuy WHERE qty = pqty;
+END;
+
+--요구사항
+--1. 100건의 데이터를 난수로 INSERT
+--2. 같은사람 + 같은물품 = 수량 UPDATE
+--3. 목록 출력
--- Class/DB/script/JDBCTest/src/com/test/jdbc/Ex08_CallableStatement.java
+++ Class/DB/script/JDBCTest/src/com/test/jdbc/Ex08_CallableStatement.java
@@ -2,10 +2,19 @@ package com.test.jdbc;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.Random;
+import java.util.Scanner;
 
 import oracle.jdbc.internal.OracleTypes;
 
 public class Ex08_CallableStatement {
+
+	private static Scanner scan;
+	
+	static {
+		scan = new Scanner(System.in);
+	}
 
 	public static void main(String[] args) {
 		
@@ -17,10 +26,136 @@ public class Ex08_CallableStatement {
 		
 		//m1();
 		//m2();
-		m3();
+		//m3();
+		//m4();
+		//m5();
+		m6();
 	}
 	
+	private static void m6() {
+		
+		Random random = new Random();
+		String[] names = {"홍길동", "아무개", "하하하", "테스트", "호호호"};
+	    String[] items = {"키보드", "마우스", "모니터", "프린터"};
+	    int qty = random.nextInt(10) + 1;
+	    
+	    String name = names[random.nextInt(5)];
+	    String item = items[random.nextInt(4)];
+	    
+	    Connection conn = null;
+	    CallableStatement stat = null;
+	    
+	    
+	    	
+		    try {
+					
+		    	conn = DBUtil.open();
+		    	
+		    	for (int i = 0; i < 100; i++) {
+	    		String sql = "{ call proc_addbuy(?, ?, ?) }";
+		    	
+		    	stat = conn.prepareCall(sql);
+	    		
+	    		stat.setString(1, names[random.nextInt(5)]);
+		    	stat.setString(2, items[random.nextInt(4)]);
+		    	stat.setInt(3, random.nextInt(10) + 1);
+		    	}
+		    	stat.executeQuery();	
+		    	
+		    	stat.close();
+		    	conn.close();
+		    	
+			} catch (Exception e) {
+				System.out.println(e.toString());
+			}
+	    
+	    System.out.println("추가 완료.");
+	    
+	    System.out.println("수량 입력 : ");
+	    String pqty = scan.nextLine();
+	    
+	    try {
+			
+	    	conn = DBUtil.open();
+    		
+    		String sql = "{ call proc_listbuy(?, ?) }";
+	    	
+	    	stat = conn.prepareCall(sql);
+	    	
+	    	stat.setString(1, pqty);
+	    	
+	    	stat.registerOutParameter(2, OracleTypes.CURSOR);
+	    	
+	    	stat.executeQuery();
+	    	
+			ResultSet rs = (ResultSet)stat.getObject(2);
+			
+			System.out.println("[번호]\t[이름]\t[물품]\t[수량]\t[날짜]");
+			while (rs.next()) {
+				
+				System.out.printf("%s\t%s\t%s\t%s\t%s\n"
+						, rs.getString("seq")
+						, rs.getString("name")
+						, rs.getString("item")
+						, rs.getString("qty")
+						, rs.getString("regdate"));
+			}
+			
+			stat.close();
+			conn.close();
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+	}
+
+	private static void m5() {
+		
+		//5. 전화번호 일부 입력 -> 직원 명단을 출력(이름, 부서, 직위, 전화번호)
+		//번호 입력 : 123
+		//proc_m5(?, ?)
+		
+		Connection conn = null;
+		CallableStatement stat = null;
+		
+		System.out.println("전화번호 검색 : ");
+		String ptel = scan.nextLine();
+		
+		try {
+			
+			conn = DBUtil.open();
+			
+			String sql = "{ call proc_m5(?, ?) }";
+			
+			stat = conn.prepareCall(sql);
+			
+			stat.setString(1, ptel);
+			
+			stat.registerOutParameter(2, OracleTypes.CURSOR);
+			
+			stat.executeQuery();
+			
+			ResultSet rs = (ResultSet)stat.getObject(2);
+			
+			System.out.println("[이름]\t[부서]\t[직위]\t[번호]");
+			while (rs.next()) {
+				
+				System.out.printf("%s\t%s\t%s\t%s\n"
+						, rs.getString("name")
+						, rs.getString("buseo")
+						, rs.getString("jikwi")
+						, rs.getString("tel"));
+			}
+			
+			stat.close();
+			conn.close();
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+	}
+
 	private static void m4() {
+		
+		//4. 커서 반환
 		
 		Connection conn = null;
 		CallableStatement stat = null;
@@ -28,6 +163,21 @@ public class Ex08_CallableStatement {
 		try {
 			
 			conn = DBUtil.open();
+			
+			String sql = "{ call proc_m4(?, ?) }";
+			stat = conn.prepareCall(sql);
+			
+			stat.setString(1, "부산");
+			
+			stat.registerOutParameter(2, OracleTypes.CURSOR);
+			
+			stat.executeQuery();
+			
+			ResultSet rs = (ResultSet)stat.getObject(2);
+			
+			while (rs.next()) {
+				System.out.println(rs.getString("name") + " - " + rs.getString("buseo"));
+			}
 			
 		} catch (Exception e) {
 			System.out.println(e.toString());
