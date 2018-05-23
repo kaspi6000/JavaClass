67894b83e20d1f79b2904243a3a10b17ae9a1207
kang <kaspi6000@gmail.com>
2018-05-23 10:14:26 +0900

    Add Spring

---
+++ Class/SpringBegin/src/com/test/spring/Ex01.java
@@ -0,0 +1,21 @@
+package com.test.spring;
+
+import java.util.Scanner;
+
+public class Ex01 {
+	
+	public static void main(String[] args) {
+		
+		// 요구사항] 사용자에게 이름을 입력받아 인사하시오.
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("이름 : ");
+		
+		String name = scan.nextLine();
+		System.out.printf("안녕하세요 %s님\n", name);
+		
+		InputData inputData = new InputData();
+		name = inputData.getName();
+		System.out.printf("안녕하세요 %s님\n", name);
+	}
+}
